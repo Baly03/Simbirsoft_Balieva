8108e5db4e0853ad3dbab0e035fa190c312c151d
Baly03 <105426344+Baly03@users.noreply.github.com>
2022-06-01 21:27:25 +0400

    Delete task_3.10/src directory

--- task_3.10/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        maxNumber maxNumber = new maxNumber();
-        maxNumber.maxNumber();
-        maxNumber.productsOfANumber();
-    }
-}

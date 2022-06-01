980f8199dcb0de794ded618cfaaa4b76e37b965c
Baly03 <105426344+Baly03@users.noreply.github.com>
2022-06-01 21:29:10 +0400

    Delete task_3.13/src directory

--- task_3.13/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        getNumber getNumber = new getNumber();
-        getNumber.getNumber();
-        getNumber.actionChoice();
-    }
-}
